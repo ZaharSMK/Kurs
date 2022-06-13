4f19232a8bc359c7f6a7b90236d8b66192bb04a3
ZaharSMK <z.a.h.a.r@mail.ru>
2022-06-13 22:28:21 +0300

    Fix

--- Lesson8.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const category = 'toys';
-
-console.log(`https://someurl.com/${category}/5`);  // косые кавычки, чтобы не использовать + для склейки
-
-const user = 'Zahar';
-
-alert(`Привет, ${user}`);
